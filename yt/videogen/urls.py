eaa66d2c4485ae35fb70cff8813ceb07758e5662
Shubh Khandelwal <78661257+caffeinelover1012@users.noreply.github.com>
2023-05-28 22:38:49 -0700

    video generation working

--- yt/videogen/urls.py
+++ yt/videogen/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('generate/', views.generate_video, name='generate_video'),
     path('myvideos/', views.user_videos, name='user_videos'),
+    path('video/<int:video_id>/', views.video_details, name='video_details'),
 ]
--- yt/videogen/views.py
+++ yt/videogen/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import VideoForm
 from .models import Video
 import openai
 
-openai.api_key = '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'
+openai.api_key = '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'
 
 
 # you can now use response.choices[0].text to get the generated text
@@ -27,13 +27,15 @@ def generate_video(request):
             engine="text-davinci-003",
             prompt="Generate a 200 word informative youtube script about the topic "+ title+ ": '{}'",
             max_tokens=word_limit
-            )['text']
+            )
+            generated_script = (list(response.choices)[0].to_dict()['text'])
+
 
             video = Video(
                 user=request.user, 
                 title=title,
                 description=description,
-                generated_script=response,
+                generated_script=generated_script,
                 tags="test, test2"
             )
             video.save()
@@ -48,3 +50,7 @@ def generate_video(request):
 def user_videos(request):
     videos = Video.objects.filter(user=request.user)
     return render(request, 'user_videos.html', {'videos': videos})
+
+def video_details(request, video_id):
+    video = get_object_or_404(Video, id=video_id)
+    return render(request, 'video_details.html', {'video': video})
