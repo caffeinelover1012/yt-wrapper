d06f19fbe72386b72dbbccef7ea829373d333ae3
Shubh Khandelwal <78661257+caffeinelover1012@users.noreply.github.com>
2023-05-28 21:26:31 -0700

    try this commit

--- yt/videogen/models.py
+++ yt/videogen/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Video(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date_of_generation = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- yt/videogen/views.py
+++ yt/videogen/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Video
+from .forms import VideoForm
+
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+# def video_view(request):
+#     if request.method == 'POST':
+#         form = VideoForm(request.POST)
+#         if form.is_valid():
+#             video = form.save(commit=False)
+#             video.user = request.user
+#             video.save()
+
+#             # Here is where you would call the GPT-3 API
+#             # You'll want to replace this with your actual API key and implementation
+#             openai.api_key = 'your-api-key'
+#             response = openai.Completion.create(
+#               engine="text-davinci-002",
+#               prompt="Translate the following English text to French: '{}'",
+#               max_tokens=60
+#             )
+
+#             return render(request, 'response.html', {'response': response.choices[0].text.strip()})
+#     else:
+#         form = VideoForm()
+#     return render(request, 'form.html', {'form': form})
